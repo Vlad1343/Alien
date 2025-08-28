14b64f091f0b448c95b882c0c1eecf9ac659e42e
Vladyslav Shutkevych <vladshutkevych@gmail.com>
2025-08-28 23:27:39 +0300

    Add Alien Invasion game files - Python game with sprites and gameplay mechanics

--- game_stats.py
+++ game_stats.py
@@ -10,7 +10,7 @@ class GameStats:
         self.game_active = False
 
         # # High score should never be reset.
-        # self.high_score = 0
+        self.high_score = 0
 
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
